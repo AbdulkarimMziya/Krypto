1936ac6fadd063ae5cec8c782a6c339eab48e9f5
AbdulkarimMziya <abdulmziya@gmail.com>
2024-01-17 17:26:01 -0800

    hero -update

--- Krypto/src/App.jsx
+++ Krypto/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.scss'
-import NavBar from './NavBar/NavBar'
-import Hero from './Hero/Hero'
+import NavBar from './Components/NavBar/NavBar'
+import Hero from './Components/Hero/Hero'
+import Logo from './Components/Logo/Logo'
 
 const App = () => {
   return (
-    <div>
+    <div className='webpage'>
       <NavBar />
       <Hero />
+      <Logo />
     </div>
   )
 }
